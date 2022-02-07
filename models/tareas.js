4d19e1dfee829d94d18a8dbef8d3fb0b31001496
Marce Serrano <smarcelo75@hotmail.com>
2022-02-07 00:53:10 -0300

    feat:m3t11

--- models/tareas.js
+++ models/tareas.js
@@ -1,3 +1,4 @@
+const colores = require('colors');
 const { Tarea } = require('../models/tarea');
 
 class Tareas {
@@ -51,6 +52,60 @@ class Tareas {
             listado += txtTarea;
         })
         return listado;
+    }
+
+    listarPendientesCompletadas(completadas = true) {
+        let indice = 0;
+        let numero = '';
+        let descripcion = '';
+        let listado = '';
+        let txtTarea = '';
+        if (completadas) {
+            Object.keys(this._listado).forEach(tarea => {
+                if (this._listado[tarea].completadoEn !== null) {
+                    indice += 1;
+                    numero = String(indice).green;
+                    descripcion = String(this._listado[tarea].desc).green;
+                    txtTarea = `${numero}. ${descripcion}\n`;
+                    listado += txtTarea;
+                }
+            })
+        } else {
+            Object.keys(this._listado).forEach(tarea => {
+                if (this._listado[tarea].completadoEn === null) {
+                    indice += 1;
+                    numero = String(indice).green;
+                    descripcion = String(this._listado[tarea].desc).red;
+                    txtTarea = `${numero}. ${descripcion}\n`;
+                    listado += txtTarea;
+                }
+            })
+        }
+        return listado;
+    }
+
+    listarPendientesCompletadas_v2(completadas = true) {
+        let criterio;
+        let numero = '';
+        let descripcion = '';
+        let txtTarea = '';
+        let txtListado = '';
+        let color;
+        if (completadas) {
+            criterio = t => t.completadoEn !== null;
+            color = colores.green;
+        } else {
+            criterio = t => t.completadoEn === null;
+            color = colores.red;
+        }
+        let listado = this.listarArr.filter(t => criterio(t));
+        for (let i = 0; i < listado.length; i += 1) {
+            numero = String(i + 1).green;
+            descripcion = color(listado[i].desc);
+            txtTarea = `${numero}. ${descripcion}\n`;
+            txtListado += txtTarea;
+        }
+        return txtListado;
     }
 }
 
